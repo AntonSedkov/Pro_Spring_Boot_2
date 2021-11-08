bb7184f27b08bcc3860204bba390ce8d69321925
User from script <S3dk0vAG@gmail.com>
2021-11-08 22:21:54 +0300

    init complex repo

---
+++ 1_Spring_todo/src/main/java/by/anthony/todo/SimpleWebApp.java
@@ -0,0 +1,12 @@
+package by.anthony.todo;
+
+public class SimpleWebApp {
+    /*public static void main(String[] args) {
+        SpringApplication.run(SimpleWebApp.class, args);
+    }
+
+    @RequestMapping("/")
+    public String greetings() {
+        return "<h1>Spring Boot Rocks in Java too!</h1>";
+    }*/
+}
